1eb16bb8c2ca04410698cf756fa2bd78472cf648
wnmMrrd <51522734+wnmMrrd@users.noreply.github.com>
2021-01-01 10:32:14 +0800

    Update pc_reg.v

--- riscv/src/pc_reg.v
+++ riscv/src/pc_reg.v
@@ -6,33 +6,21 @@ module pc_reg(
 
     input wire[`StallBus] stall_state,
 
-    input wire ex_be_i,
-    input wire[`InstAddrBus] ex_bto_i,
+    input wire jump_flag,
+    input wire[`InstAddrBus] jump_to,
 
-    input wire je,
-    input wire jto,
-
-    output reg[`InstAddrBus] pc,
-    output reg jmp
+    output reg[`InstAddrBus] pc
 );
 
     always @ (posedge clk) begin
         if(rst == `RstEnable) begin
             pc <= `ZeroWord;
-            jmp <= `False_v;
-        end else if(stall_state[1] == `True_v) begin
-            
-        end else if(ex_be_i == `True_v) begin
-            pc <= ex_bto_i;
-            jmp <= `False_v;
+        end else if(jump_flag == `True_v) begin
+            pc <= jump_to;
         end else if(stall_state[0] == `True_v) begin
             
-        end else if(je == `True_v) begin
-            pc <= jto;
-            jmp <= `True_v;
         end else begin
             pc <= pc+4;
-            jmp <= `False_v;
         end
     end
 
