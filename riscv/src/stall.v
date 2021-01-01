5d97748e46eddef44e438b46b6a80daac897a5a6
wnmMrrd <51522734+wnmMrrd@users.noreply.github.com>
2021-01-01 10:34:16 +0800

    Update stall.v

--- riscv/src/stall.v
+++ riscv/src/stall.v
@@ -2,23 +2,24 @@
 
 module stall(
     input wire rst,
+
     input wire if_stall,
     input wire id_stall,
     input wire mem_stall,
-    output reg stall_state
+    output reg[4:0] stall_state
 );
 
     always @ (*) begin
         if(rst == `RstEnable) begin
-            state_state <= `AllStall;
+            stall_state = `AllStall;
         end else if(mem_stall == `True_v) begin
-            stall_state <= `MemStall;
+            stall_state = `MemStall;
         end else if(id_stall == `True_v) begin
-            stall_state <= `IdStall;
+            stall_state = `IdStall;
         end else if(if_stall == `True_v) begin
-            stall_state <= `IfStall;
+            stall_state = `IfStall;
         end else begin
-            stall_state <= `NoStall;
+            stall_state = `NoStall;
         end
     end
 
