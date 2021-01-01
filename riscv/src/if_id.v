0f9d5286963cf24542c1e4895d13c35114aa1d86
wnmMrrd <51522734+wnmMrrd@users.noreply.github.com>
2021-01-01 10:32:28 +0800

    Update if_id.v

--- riscv/src/if_id.v
+++ riscv/src/if_id.v
@@ -7,8 +7,7 @@ module if_id(
     input wire[`InstAddrBus] if_pc,
     input wire[`InstBus] if_inst,
 
-    input wire ex_be_i,
-    
+    input jump_flag,
     input  wire[`StallBus] stall_state,
 
     output reg[`InstAddrBus] id_pc,
@@ -16,20 +15,18 @@ module if_id(
 );
 
     always @ (posedge clk) begin
-        if(rst == `RstEnable) begin
+        if(rst == `RstEnable || jump_flag == `True_v) begin
             id_pc <= `ZeroWord;
             id_inst <= `ZeroWord;
         end else if(stall_state[2] == `True_v) begin
             
-        end else if(ex_be_i == `True_v) begin
+        end else if(stall_state[1] == `True_v) begin
             id_pc <= `ZeroWord;
             id_inst <= `ZeroWord;
-        end else if(stall_state[0] == `False_v) begin
-            id_pc <= if_pc;
-            od_inst <= if_inst;
         end else begin
             id_pc <= if_pc;
             id_inst <= if_inst;
         end
     end
+
 endmodule
